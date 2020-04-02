6809644bd01defefd87d473a90ad0f994c610ed3
mydnac <mandycoelho5@gmail.com>
2020-04-02 12:52:17 +0200

    second version

--- cookingRecipe.ts
+++ cookingRecipe.ts
@@ -1,28 +1,31 @@
-class Recipe {
-    name : string;
-    type: string;
-    preparationTime : number;
+interface Ingredient {
+    nameIngredient : string,
+    priceKg : number,
+    imgUrl : string 
+}
 
-    constructor(name: string,type: string, preparationTime : number) {
-        this.name = name;
-        this.type = type;
-        this.preparationTime = preparationTime
-
-
+class Recipe{
+    constructor(public name: string) {
+}
 };
 
-class Ingredient extends Recipe {
-    nameIngredient : string;
-    priceKg : number;
-    urlImg : string;
-
-    constructor(name: string,type: string, preparationTime : number, nameIngredient: string, priceKg: number, urlImg ) {
-        super(name, type, preparationTime)
-        this.nameIngredient = nameIngredient;
-        this.priceKg = priceKg;
-        this.urlImg = urlImg;
-    }
-    
+class Starter extends Recipe implements Ingredient {
+    constructor (public name: string, public nameIngredient: string, public priceKg: number, public imgUrl : string, public preparationTime: number ) {
+    super(name)
 }
+};
+
+class MainCourse extends Recipe implements Ingredient{
+    constructor (public name: string, public nameIngredient: string, public priceKg: number, public imgUrl : string, public preparationTime: number ) {
+    super(name)
+}
+};
+
+class Dessert extends Recipe implements Ingredient{
+    constructor (public name: string, public nameIngredient: string, public priceKg: number, public imgUrl : string, public preparationTime: number ) {
+    super(name)
+}
+};
+
 
 
