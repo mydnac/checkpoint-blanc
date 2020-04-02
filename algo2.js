9a89ed4564f066b904ae4da418ba85b881d12f99
mydnac <mandycoelho5@gmail.com>
2020-04-02 09:36:34 +0200

    resolution algo2

--- algo2.js
+++ algo2.js
@@ -1,5 +1,9 @@
-const numberList = [20,30,1,4,9,20]
+const numberList = [20,30,1,4,9,20];
 
+function sortArray(numberList) {
+    return numberList.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
+}
+console.log(sortArray(numberList))
 
 
 // Ecrire une fonction prend en entrée un tableau de nombre (numberList) et qui retourne le tableau triée
